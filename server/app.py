12ecfc06c6e99e8298ccacdbf08c54b4f3b357b8
visvesh123 <visveshnaraharisetty@gmail.com>
2021-11-29 19:29:50 +0530

    First commit

---
+++ server/app.py
@@ -0,0 +1,95 @@
+from flask import Flask, jsonify, request
+import numpy as np
+import pandas as pd
+from keybert import KeyBERT
+
+
+model = KeyBERT(model="distilbert-base-nli-mean-tokens")
+
+app = Flask(__name__)
+
+
+df = pd.concat(
+    map(pd.read_csv, ['reviews-13495-13500.csv', 'reviews-13500-13537.csv']), ignore_index=True)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if(request.method == 'GET'):
+
+        data = "hello world"
+        return jsonify({'data': data})
+
+
+@app.route('/home/<int:num>', methods=['GET'])
+def disp(num):
+    print(num)
+    return jsonify({'data': num**2})
+
+
+# No of reviews
+@app.route('/no_of_reviews/<int:appid>')
+def avg_playtime(appid):
+
+    df3 = df[(df['appid'] == appid)]
+    x = df3.shape[0]
+    print(x)
+    return jsonify({'no_of_reviews': x})
+
+
+# True , False sentiments
+@app.route('/review_sentiments/<int:appid>')
+def rev_sentiments(appid):
+
+    df3 = df[(df['appid'] == appid)]
+
+    y = df3['voted_up'].value_counts().tolist()
+    print(y)
+    return jsonify({'sentiment': y})
+
+
+# recent reviews
+
+
+@app.route('/recent_reviews/<int:appid>')
+def recent_reviews(appid):
+
+    df3 = df[(df['appid'] == appid)]
+
+    sorted_df = df3.sort_values('unix_timestamp_created', ascending=[False])
+    z = sorted_df.values.tolist()
+    return jsonify({'Recent_reviws': z})
+
+# trending keywords
+
+
+@app.route('/trending_keywords/<int:appid>')
+def trending_keywords(appid):
+
+    df3 = df[(df['appid'] == appid)]
+    x = df3.shape[0]
+    arr = []
+    for i in range(1, 100):
+        a = model.extract_keywords(
+            df3['review'].values[i], top_n=1, keyphrase_ngram_range=(1, 1), stop_words="english")
+        arr = arr + a
+    x = arr.sort(key=lambda x: x[1])
+    print(x)
+    return jsonify({'Trending keywords': arr})
+
+# Average rating of game
+
+
+@app.route('/avg_rating/<int:appid>')
+def avg_rating(appid):
+
+    df3 = df[(df['appid'] == appid)]
+    x = df3['voted_up'].value_counts().tolist()
+    rate = (x[0] * 5 + (x[1])) / (x[0]+x[1])
+    return jsonify({'Average Rating': rate})
+
+
+# driver function
+if __name__ == '__main__':
+
+    app.run(debug=True)
